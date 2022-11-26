19bf54bb88239499fdf556d164df903eb3eeb70b
vjatla <venkatesh.jatla@gmail.com>
2022-11-26 14:22:48 -0800

    A workable emacs without personal keybindings

---
+++ init.el
@@ -0,0 +1,5 @@
+;; Loading literate config files
+(org-babel-load-file "~/.emacs.d/package-installer.org")
+(org-babel-load-file "~/.emacs.d/looks.org")
+(org-babel-load-file "~/.emacs.d/feel.org")
+(org-babel-load-file "~/.emacs.d/lsp.org")
