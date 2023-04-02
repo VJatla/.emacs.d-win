be49b07edd2cffc1545a43c89821bc81d5aa3c35
VJ-UNM@Aurora-Win <venkatesh369@unm.edu>
2023-04-02 06:19:09 -0700

    Now supporting latex in widnows with help of SumatraPDF

--- init.el
+++ init.el
@@ -7,13 +7,16 @@
 (org-babel-load-file "~/.emacs.d/python.org")
 (org-babel-load-file "~/.emacs.d/markdown.org")
 (org-babel-load-file "~/.emacs.d/keybindings.org")
+(org-babel-load-file "~/.emacs.d/latex.org")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((eval pyvenv-activate "~/anaconda3/envs/haq")))
+ '(safe-local-variable-values
+   '((TeX-master . t)
+     (eval pyvenv-activate "c:/Users/VJ-UNM/anaconda3/envs/haq")))
  '(warning-suppress-log-types '((emacs) (emacs)))
  '(warning-suppress-types '((emacs))))
 (custom-set-faces
